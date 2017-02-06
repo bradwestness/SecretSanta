7e4ba2d805416f9dc7e146e40a0ca2a63a007bac
Brad Westness <brad.westness@outlook.com>
2017-02-05 22:49:38 -0600

    Disable SSL for sending e-mails

--- SecretSanta/Utilities/EmailMessage.cs
+++ SecretSanta/Utilities/EmailMessage.cs
@@ -18,7 +18,7 @@ namespace SecretSanta.Utilities
                 message.Subject = subject;
                 message.Body = new TextPart(TextFormat.Html) { Text = body.Replace(Environment.NewLine, "<br />") };
 
-                smtp.Connect(AppSettings.SmtpHost, AppSettings.SmtpPort, true);
+                smtp.Connect(AppSettings.SmtpHost, AppSettings.SmtpPort, false);
                 smtp.Authenticate(AppSettings.SmtpUser, AppSettings.SmtpPass);
                 smtp.Send(message);
                 smtp.Disconnect(true);
