0531a8dfefd54c545cb8659a2f4cd356285f1d70
Brad Westness <brad.westness@outlook.com>
2020-11-26 22:50:24 -0600

    initialize datarepository and previewgenerator

--- SecretSanta/Startup.cs
+++ SecretSanta/Startup.cs
@@ -46,6 +46,9 @@ namespace SecretSanta
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			DataRepository.Initialize(env.ContentRootPath);
+			PreviewGenerator.Initialize(env.WebRootPath);
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
