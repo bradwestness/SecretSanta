61e7f047d1dfd67fd7239c91df0c4409d5682110
Brad Westness <brad.westness@carvana.com>
2021-11-17 20:36:12 -0600

    modal changes

--- SecretSanta/Models/AccountModels.cs
+++ SecretSanta/Models/AccountModels.cs
@@ -73,7 +73,7 @@ public class EditUserModel
 
     public Guid? Picked { get; set; }
 
-    [DisplayName("PickedBy")]
+    [DisplayName("Picked By")]
     public string? PickedBy { get; set; }
 
     [DisplayName("Do Not Pick")]
